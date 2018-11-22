1aacab871b290302cb16f06f8a9baf75426a9efe
Cindy Wang <xw2368@columbia.edu>
2018-11-22 19:14:18 +0000

    Added code which is adapted from colorNet pytorch implementation

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Image-Colorization
-This is the a Computer Vision project on image colorization.
+# colorNet-pytorch
+A Neural Network For Automatic Image Colorization
+
+This project is a PyTorch version of the [ColorNet](http://hi.cs.waseda.ac.jp/~iizuka/projects/colorization/en/) issued on SIGGRAPH 2016. Please check out the original website for the details.
+
+## Overview
+* Net model
+![...](https://github.com/shufanwu/colorNet-pytorch/blob/master/readme%20images/model.png)
+
+* DataSet
+[MIT Places205](http://places.csail.mit.edu/user/index.php)  
+> Hint: For there are grayscale images in the dataset, I write a script to remove these images
+
+* Development Environment  
+Python 3.5.1  
+CUDA 8.0  
+
+## Result
+I just train this model for 3 epochs while 11 epochs in the paper， so I think it will work better if train it more.
+
+* Good results  
+![...](https://github.com/shufanwu/colorNet-pytorch/blob/master/readme%20images/good-result.png)  
+* Bad results  
+![...](https://github.com/shufanwu/colorNet-pytorch/blob/master/readme%20images/bad-result.png)  
+For this network is trained by landscape image database, it's work well for scenery pictures. So if you use this network to color  images of other types, maybe you can't get a satisfying output.
+
+
---
+++ train.py
@@ -0,0 +1,89 @@
+import os
+import traceback
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torchvision import transforms
+import numpy as np
+
+from myimgfolder import TrainImageFolder
+from colornet import ColorNet
+
+original_transform = transforms.Compose([
+    transforms.Scale(256),
+    transforms.RandomCrop(224),
+    transforms.RandomHorizontalFlip(),
+    #transforms.ToTensor()
+])
+
+have_cuda = torch.cuda.is_available()
+epochs = 3
+
+data_dir = "./places365_standard/train/"
+train_set = TrainImageFolder(data_dir, original_transform)
+train_set_size = len(train_set)
+train_set_classes = train_set.classes
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True, num_workers=4)
+color_model = ColorNet()
+if os.path.exists('./pretrained/colornet_params.pkl'):
+    color_model.load_state_dict(torch.load('./pretrained/colornet_params.pkl'))
+if have_cuda:
+    color_model.cuda()
+optimizer = optim.Adadelta(color_model.parameters())
+
+if have_cuda: 
+    print ("Have cuda")
+else:
+    print ("No cuda")
+
+def train(epoch):
+    color_model.train()
+
+    try:
+        for batch_idx, (data, classes) in enumerate(train_loader):
+            print ('batch_idx: %d' %batch_idx)
+            messagefile = open('./message.txt', 'a')
+            original_img = data[0].unsqueeze(1).float()
+            img_ab = data[1].float()
+            if have_cuda:
+                original_img = original_img.cuda()
+                img_ab = img_ab.cuda()
+                classes = classes.cuda()
+            original_img = Variable(original_img)
+            img_ab = Variable(img_ab)
+            classes = Variable(classes)
+            optimizer.zero_grad()
+            class_output, output = color_model(original_img, original_img)
+            if have_cuda:
+                ems_loss = torch.pow((img_ab - output), 2).sum() / Variable(torch.from_numpy(np.array(list(output.size()))).cuda()).prod().float()
+            else:
+                ems_loss = torch.pow((img_ab - output), 2).sum() / Variable(torch.from_numpy(np.array(list(output.size())))).prod().float()
+            cross_entropy_loss = 1/300 * F.cross_entropy(class_output, classes)
+            loss = ems_loss + cross_entropy_loss
+            lossmsg = 'loss: %.9f\n' % (loss.item())
+            messagefile.write(lossmsg)
+            ems_loss.backward(retain_graph=True)
+            cross_entropy_loss.backward()
+            optimizer.step()
+            if batch_idx % 500 == 0:
+                message = 'Train Epoch:%d\tPercent:[%d/%d (%.0f%%)]\tLoss:%.9f\n' % (
+                    epoch, batch_idx * 32, len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item())
+                messagefile.write(message)
+                torch.save(color_model.state_dict(), 'colornet_params_%d.pkl' %epoch)
+            messagefile.close()
+                # print('Train Epoch: {}[{}/{}({:.0f}%)]\tLoss: {:.9f}\n'.format(
+                #     epoch, batch_idx * len(data), len(train_loader.dataset),
+                #     100. * batch_idx / len(train_loader), loss.data[0]))
+    except Exception:
+        logfile = open('log.txt', 'w')
+        logfile.write(traceback.format_exc())
+        logfile.close()
+    finally:
+        torch.save(color_model.state_dict(), 'colornet_params.pkl')
+
+
+for epoch in range(2, epochs + 1):
+    train(epoch)
